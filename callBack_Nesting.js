854e7463e0ebc82613223ff72adde1d2bbba4acd
Ashish <ash.ishu025@gmail.com>
2023-11-01 21:44:21 +0530

    nested functions

--- callBack_Nesting.js
+++ callBack_Nesting.js
@@ -12,16 +12,47 @@ function GetDetails(userName, uAddress, CallBackFunction) {
     CallBackFunction(userName, uAddress)
 }
 
-GetDetails("Bryan", "Somewhere on earth", PrintDetails)
+//GetDetails("Bryan", "Somewhere on earth", PrintDetails)
 
 function AccountDetails(accType, accBal, CallBackFunction) {
     CallBackFunction(accType, "$100")
 }
 
-AccountDetails("Credit", "$1000", PrintDetails)
+//AccountDetails("Credit", "$1000", PrintDetails)
 
 
 // create a function arithmatic calculation
 // inside that pass, two params
 // create 4 different functions - Sum, Substract, Multiply and Divide
 // If we pass Sum it should do Sum, same way Substract, Multiply and Divide
+
+
+//nested function and javascript currying (arrange raw, )
+
+var number1 = 20;
+
+function A(a) {
+    //console.log(d) //- scope of variables decrease when we go up
+    
+    return function B(b) {
+        
+        return function C(c) {
+            
+            return function D(d) {
+                   return  a + b + c + d + number1
+            }
+        }
+    }   
+}
+
+var b = A(5)
+var c = b(5)
+var d = c(5)
+var result = d(5)
+
+console.log(result)
+
+//chain execution
+
+var chainResult = A(10)(10)(10)(10)
+console.log(chainResult)
