c4fca8c5a6d527aa5db47f2af953e03af1a8da5c
Ashish <ash.ishu025@gmail.com>
2023-11-30 20:05:53 +0530

    es6 assessment

---
+++ ES6/es6_assessment.js
@@ -0,0 +1,42 @@
+// 1. How to preserve the immutability on my heroes list? Solve below problems using the same
+// a. Get heroes who are not evils
+// b. Print Unique family names
+// c. Print Hero Names from given objects, and append sir in each of them before printing
+// d. Do we have any hero in Marvel Family who is not evil
+
+// const heroes = [
+//   { name: 'Wolverine',      family: 'Marvel',    isEvil: false },
+//   { name: 'Deadpool',       family: 'Marvel',    isEvil: false },
+//   { name: 'Magneto',        family: 'Marvel',    isEvil: true  },
+//   { name: 'Charles Xavier', family: 'Marvel',    isEvil: false },
+//   { name: 'Batman',         family: 'DC Comics', isEvil: false },
+//   { name: 'Harley Quinn',   family: 'DC Comics', isEvil: true  },
+//   { name: 'Legolas',        family: 'Tolkien',   isEvil: false },
+//   { name: 'Gandalf',        family: 'Tolkien',   isEvil: false },
+//   { name: 'Saruman',        family: 'Tolkien',   isEvil: true  }
+// ]
+
+//2. Give me an example of map and set collection each with at least four properties implemented - like get, set, clear, etc
+
+//3. Create a promise object that get resloved after two seconds and rejected after three. Also it returns five ES6 features on resolved
+
+//4. Use the spread and rest operator to create a function which can multiple numbers from 1...n (n is the number of choice)
+//   also need to print students of the session using same example
+
+//5. Print the last name through destructuring and add a contact number:9119119110 as well
+// const person = {
+//     userDetails :{
+//         first: "FirstName",
+//         last: "LastName"
+//     }
+// }
+
+//6. Give me an example of const data manipulation
+
+//7. What is the difference between for-of and for-in show with examples
+
+//8. Give me an example of bind and write its usage, comparison with arrow function
+
+//9. Create an example showing usage of event loop in concurrent execution cycle
+
+//10. create an example showing usage of short hand, default param and template literal.
