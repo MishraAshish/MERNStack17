f84056a95a8648b1f8121d63c64434d57695b13c
Ashish <ash.ishu025@gmail.com>
2023-12-12 22:44:07 +0530

    webpack

---
+++ ES6/front-end.js
@@ -0,0 +1,25 @@
+//Backend - Node+Express API => JSON data, JSON will be consumed on front end
+//localhost:9000/api/user/siginup => userid/ {userinfo}
+
+
+//FrontEnd - React, library
+//localhost:9090/home
+//localhost:9090/login
+//localhost:9090/about
+// header + footer
+
+//webpack-dev-server - job#1
+//webpack - development
+
+//1. bundling multiple files generated for different screens (creates a new bundle of all component.js => bundle.js)
+//2. minification of files - removing spaces, changing smart logical replacements, removing commmented code, redundent code
+
+// let stmt = "2. minification of files"
+//stmt stmt stmt stmt
+//stmt stmt stmt
+//stmt stmt stmt
+
+//3. minification of files - js, jsx(react file), css, images etc
+//4. transpilation using babel-loaders - es6, react, ==> converting code written in ES6/7/8/nxt, react into core js 
+//5. support for developers to debug by having mutiple versions for production support
+//6. monitoring, rebuilding and hot-reloading of application with changes on the fly
