a8c11ddb11613d0f7f747003619c42530dfe375a
Ashish <ash.ishu025@gmail.com>
2023-12-13 22:40:48 +0530

    setup webpack babel react

---
+++ reactapp/src/index.js
@@ -0,0 +1,15 @@
+//console.log("Shopping Cart Application - To be Built on React will be loaded through this file!!!")
+//alert("Index js file loaded")
+
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+
+import ApplicationComponent from "./Application/app";
+
+//creating root of the react application where we can load the react app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//bootstrapping react application in root element of index.html
+root.render( //this creates the virtual dom (copy of actual dom in js object format)
+    <ApplicationComponent/>
+)
