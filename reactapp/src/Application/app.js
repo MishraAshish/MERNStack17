91da0de61b9223cba64a73330754cde4bf9724d1
Ashish <ash.ishu025@gmail.com>
2023-12-18 21:52:19 +0530

    react assessment 1

--- reactapp/src/Application/app.js
+++ reactapp/src/Application/app.js
@@ -4,11 +4,13 @@ import HeaderComponent from "./Common/headerComponent"
 
 let ApplicationComponent = ()=>{
     let a = 10, b = 5;
+    let header = "10,001+ employees · UI Specialist"
     return(
         <>
             <HeaderComponent/>
             <b>The Arithmatic Operation {a+b} {a*b} {a-b} {a/b}</b>
             <h1>This is the Application Component</h1>
+            <h1>{header}</h1>
         </>
     )
 }
