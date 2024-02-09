62874b570c0372deb5bbc45281c184a244603540
Ashish <ash.ishu025@gmail.com>
2024-02-09 22:16:13 +0530

    notifications and review page

--- reactapp/src/Application/Common/ProjectEnhancement.js
+++ reactapp/src/Application/Common/ProjectEnhancement.js
@@ -1,4 +1,33 @@
-// 07-10-2023 - Reorder Page
+// 12-02-2023 - Notifications
+
+// Show notification on top of header (Right Corner)
+// Design Should show a circular or any small icon, to show number of notifications
+// Upon click it should expand in rectangular box and show all the notification messages 
+// All notification messages should be clickable
+// Upon click of any of them it should reduce the count in notification icon
+// It should have two types of notifications - static and dynamic
+// Static Notifications To Assist User
+// 1. To Add Products from Product Screen
+// 2. To Add Items from Cart Page
+// 3. To review cart from Checkout Page
+// 4. To Make Payment from Payment Page
+// 5. To Assist Them for cancel/reorder
+// Dynamic Notifications To Assist User
+// 1. To Check the number of items added in the Cart
+// 2. If user cancels an order it should it should add one notification that an order has been cancelled and add it to notification row
+
+// 12-02-2024 - Logout
+// LogOut : Set up logout button/link on TopRight corner, upon click user should be cleared and back to home
+
+// 09-02-2024 - Review Page
+// This should get its reviews from recent orders page
+// User should be allowed to give ratings and his comments to each products
+// Upon successful submission each product should have a link to show its review 
+// When user expands product detail we should also see the button to which will take us to its review
+// on recent order page we can show a popup to submit rating or a new page its up to you //can use -> react bootstrap
+// user should only be able to give rating once cancel button is gone
+
+// 07-02-2024 - Reorder Page
 // Reorder
 // User to reorder from recent orders or from cancelled orders
 // A Simple process just add the order to your cart and replace or merge whatever is present in cart
